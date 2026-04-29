20138eb9d888bc1263176eef09f93732e93e2342
Alexander Youl <formant56@hotmail.com>
2026-04-30 01:35:48 +0300

    changed time to 2 hours

--- main.py
+++ main.py
@@ -242,7 +242,7 @@ def main() -> None:
         print("Clicked the first content link under the nested accordion.")
 
  
-        for step in range(35):
+        for step in range(13):
             click_button_in_content_frame(
                 page,
                 (
@@ -253,9 +253,9 @@ def main() -> None:
                 "Next",
             )
             page.wait_for_load_state("networkidle")
-            print(f"Clicked Next button {step + 1}/35.")
+            print(f"Clicked Next button {step + 1}/13.")
 
-            if step < 59:
+            if step < 13:
                 page.wait_for_timeout(600_000)
 
         page.locator('a.nav-link.nav-pill.avatar').click()
